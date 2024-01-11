7baa898847d73dfd0225a7308b3adbca68330fae
Antoni Santandreu Sureda <santandreutoni@gmail.com>
2024-01-11 23:45:40 +0100

    Add otel using python auto instrumentation to petrol-reports

--- petrol-reports/app.py
+++ petrol-reports/app.py
@@ -1,3 +1,6 @@
+import http
+import logging
+
 from concurrent.futures import ThreadPoolExecutor
 from flask import Flask, jsonify, request
 from petrolstations.geocategory import GeoCategory
@@ -6,7 +9,6 @@ from petrolstations.product import Product
 from reports.filter import Filter
 from reports.prices_listener import PricesListener
 from reports.reports_generator import ReportsGenerator
-import logging
 
 logging.basicConfig(level=logging.INFO)
 app = Flask(__name__)
@@ -31,10 +33,10 @@ def reports(geocategory: str, product: str):
         geocategory_enum = GeoCategory(geocategory)
         product_enum = Product(product)
         if geocategory_enum == GeoCategory.CITY:
-            return jsonify({'error': 'Report by city is not available'}), 400
+            return jsonify({'error': 'Report by city is not available'}), http.HTTPStatus.BAD_REQUEST
 
     except ValueError:
-        return jsonify({'error': 'Invalid request'}), 400
+        return jsonify({'error': 'Invalid request'}), http.HTTPStatus.BAD_REQUEST
 
     # Get the 'include_outliers' query parameter, using type=bool always evaluates to True, so we
     # have to do this manual comparison
@@ -42,7 +44,7 @@ def reports(geocategory: str, product: str):
 
     f = Filter(geocategory_enum, product_enum, include_outliers)
     m = reports_generator.reports_by_filter[f]
-    return m.get_root().render()
+    return m.get_root().render(), http.HTTPStatus.OK
 
 
 if __name__ == '__main__':
